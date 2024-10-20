d6dda4d2d8325470042a0ba40bca075b3a42bce9
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-20 22:30:41 +0200

    debugging

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,24 +1,25 @@
 // Configuring MySQL
 
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
- // Testing database connection
+// Testing database connection
+console.log(process.env.DB_PASS);
 try {
-    await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  await sequelize.authenticate();
+  console.log("Connection to the database has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
-  export default sequelize;
+export default sequelize;
