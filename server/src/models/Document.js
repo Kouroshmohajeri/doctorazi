ffdd8cccb0deada72fc26127aec631dba9349028
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-20 23:25:05 +0200

    Documents model changed from lower case to uppercase

--- server/src/models/Document.js
+++ server/src/models/Document.js
@@ -1,64 +1,68 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
-import User from './User.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+import User from "./User.js";
 
-const Document = sequelize.define('Document', {
+const Document = sequelize.define(
+  "Document",
+  {
     documentId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     aboutUsEnglish: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     aboutUsFarsi: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     contactUsEnglish: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     contactUsFarsi: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     termsAndConditionsEnglish: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     termsAndConditionsFarsi: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     privacyPolicyEnglish: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     privacyPolicyFarsi: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     userId: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'user_id',
-        },
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "user_id",
+      },
+      allowNull: false,
     },
     createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
     },
     updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-    }
-}, {
-    tableName: 'documents',
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "Documents",
     timestamps: true,
-});
+  }
+);
 
 export default Document;
