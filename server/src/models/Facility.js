199fa851d27de3f9ae80ad9a6a734d7e5ab52ef7
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-20 23:51:16 +0200

    Facility name fixed

--- server/src/models/Facility.js
+++ server/src/models/Facility.js
@@ -1,33 +1,37 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
 
-const Facility = sequelize.define('Facility', {
-  facility_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  doctor_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'doctors',
-      key: 'doctor_id',
+const Facility = sequelize.define(
+  "Facility",
+  {
+    facility_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    doctor_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "doctors",
+        key: "doctor_id",
+      },
+    },
+    name: {
+      type: DataTypes.STRING(250),
+      allowNull: false,
+    },
+    position: {
+      type: DataTypes.STRING(250),
+      allowNull: false,
     },
   },
-  name: {
-    type: DataTypes.STRING(250),
-    allowNull: false,
-  },
-  position: {
-    type: DataTypes.STRING(250),
-    allowNull: false,
-  },
-}, {
-  tableName: 'facilities',
-  timestamps: true,
-  createdAt: 'createdAt',
-  updatedAt: 'updatedAt',
-});
+  {
+    tableName: "Facilities",
+    timestamps: true,
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
+  }
+);
 
 export default Facility;
