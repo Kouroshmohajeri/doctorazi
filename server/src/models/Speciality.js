568ab79ace7271446032220bd9832ee3461d242a
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-20 23:49:11 +0200

    Speciality name fixed

--- server/src/models/Speciality.js
+++ server/src/models/Speciality.js
@@ -1,24 +1,28 @@
 // Speciality model
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
 
-const Speciality = sequelize.define('Speciality', {
-  speciality_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Speciality = sequelize.define(
+  "Speciality",
+  {
+    speciality_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    translate: {
+      type: DataTypes.STRING,
+      allowNull: true, // Allow null to indicate no translation
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  translate: {
-    type: DataTypes.STRING,
-    allowNull: true, // Allow null to indicate no translation
-  },
-}, {
-  tableName: 'specialities',
-});
+  {
+    tableName: "Specialities",
+  }
+);
 
 export default Speciality;
