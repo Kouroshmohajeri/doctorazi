ddfc91e759efcaf4a005f5d24ad1e5d5c246b218
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-20 17:24:00 +0200

    server ip updated

--- client/app/api/server.js
+++ client/app/api/server.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "http://62.60.204.118/:8800/api",
   withCredentials: true,
   timeout: 10000, // 10 seconds timeout
 });
 
 // Example of using an interceptor for response error handling
 API.interceptors.response.use(
-  response => response,
-  error => {
+  (response) => response,
+  (error) => {
     // Handle errors
     console.error("An API error occurred", error);
     return Promise.reject(error);
