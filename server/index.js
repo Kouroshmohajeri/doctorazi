61378f8dcce805a968d874dbdca43dcbc1dee53b
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-24 00:01:50 +0200

    +

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: ["doctorazi.com"], // Your frontend domain
+  origin: ["https://doctorazi.com", "https://www.doctorazi.com"], // Your frontend domain
   credentials: true, // Allow cookies to be sent
 };
 
