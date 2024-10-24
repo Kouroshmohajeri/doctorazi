cae7b33b942ebf5e21ddb6ad30fa06916f11fb9b
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-10-24 12:29:17 +0200

    url updated

--- client/app/api/server.js
+++ client/app/api/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://doctorazi.com/api",
+  baseURL: "https://www.doctorazi.com/api",
   withCredentials: true,
   timeout: 10000, // 10 seconds timeout
 });
