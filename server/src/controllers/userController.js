b46d109eb5aac520d0b1c3fc82b507c865d643f0
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-11-04 01:02:13 +0100

    error checking

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -52,7 +52,7 @@ class UserController {
         password: hashedPassword,
         user_type: 1,
       });
-
+      console.log("new user Id:", newUser.id);
       // Generate JWT token for user's session
       const token = jwt.sign(
         {
